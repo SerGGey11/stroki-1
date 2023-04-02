63444e9703e9d61108a8d84e9f58cf2620c4155d
ulyana <bsergeybabak2000@gmail.com>
2023-04-02 13:39:41 +0300

    второй комит

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main(String[] args){
         task1();
+        task2();
     }
     public static void task1(){
     String firstName = "Ivan ";
     String middleName = "Ivanov ";
     String lastName = "Ivanovich";
     String fullName = middleName + firstName + lastName;{
-            System.out.println(fullName);
+            System.out.println("ФИО сотрудника - " + fullName);
     }
-}}
+}
+public static void task2(){
+   String fullName = "Ivanov Ivan Ivanovich";
+   fullName = fullName.replace("vanov ","VANOV ");
+   fullName = fullName.replace("van", "VAN");
+   fullName = fullName.replace("ovich", "OVICH");{
+        System.out.println("Данные ФИО сотрудника для заплнения отчета - " + fullName);
+    }
+}
+}
