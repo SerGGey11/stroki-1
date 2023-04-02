78249c475bd2d4ab52a2ce013590e0afd611e3e2
ulyana <bsergeybabak2000@gmail.com>
2023-04-02 13:22:00 +0300

    первый комит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        task1();
     }
-}
+    public static void task1(){
+    String firstName = "Ivan ";
+    String middleName = "Ivanov ";
+    String lastName = "Ivanovich";
+    String fullName = middleName + firstName + lastName;{
+            System.out.println(fullName);
+    }
+}}
